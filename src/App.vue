dd5b5b9be2e5d5cf43b44412e07f44d4f155af48
karan.shgl96@gmail.com <karan.shgl96@gmail.com>
2017-06-05 05:19:14 +0530

    Connected components

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
-
+<div class="container">
+  <quote-grid :quotes="quotes"></quote-grid>
+</div>
 </template>
 
 <script>
+import QuoteGrid from './components/QuoteGrid.vue';
 export default {
   data:function(){
     return {
-        quotes: [],
+        quotes: ['Hello'],
         maxQuotes: 10
+    }},
+    components:{
+      quoteGrid: QuoteGrid
     }
 
-  }
 }
 </script>
 
