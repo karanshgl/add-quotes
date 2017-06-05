6d173ec3f12bbbcc51a63e9adac4939ce4c6f5f9
karan.shgl96@gmail.com <karan.shgl96@gmail.com>
2017-06-05 18:10:17 +0530

    Removed the test quote

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@ import Header from './components/Header.vue';
 export default {
   data:function(){
     return {
-        quotes: ['Hello'],
+        quotes: [],
         maxQuotes: 10,
     }},
     components:{
